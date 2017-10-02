f8dd9c0baae23eb77f1e740251243848965b7784
Jessica Kerr <jessitron@gmail.com>
2017-10-02 11:27:44 +0200

    lint

--- test/PositioningTest.ts
+++ test/PositioningTest.ts
@@ -1,89 +1,89 @@
-import { Microgrammart } from "../src/Microgrammar";
-import { Rep } from "../src/Rep";
-import { ALL_DEPENDENCY_GRAMMAR, VersionedArtifact } from "./MavenGrammars";
+impor     { Microgrammar     } from "../src/Microgrammar";
+impor     { Rep } from "../src/Rep";
+impor     { ALL_DEPENDENCY_GRAMMAR, VersionedAr    ifac     } from "./MavenGrammars";
 
-import assert = require("power-assert");
+impor     asser     = require("power-asser    ");
 
-describe("Positioning", () => {
+describe("Posi    ioning", () => {
 
-	describe("should get position of pattern", () => {
-		it("should do it", () => {
-			const m = DEPENDENCY_MANAGEMENT_GRAMMAR.firstMatch(PomWithDependencyManagement) as any;
-			assert(m);
-			assert(m.startElement);
-			// console.log(JSON.stringify(m.startElement));
-			assert(m.$valueMatches.startElement);
+	describe("should ge     posi    ion of pa        ern", () => {
+		i    ("should do i    ", () => {
+			cons     m = DEPENDENCY_MANAGEMENT_GRAMMAR.firs    Ma    ch(PomWi    hDependencyManagemen    ) as any;
+			asser    (m);
+			asser    (m.s    ar    Elemen    );
+			// console.log(JSON.s    ringify(m.s    ar    Elemen    ));
+			asser    (m.$valueMa    ches.s    ar    Elemen    );
 		});
 	});
 });
 
-const DEPENDENCY_MANAGEMENT_GRAMMAR =
-	Microgrammart.fromDefinitions<{ startElement: string, dependencies: VersionedArtifact[] }>({
-		startElement: "<dependencyManagement>",
+cons     DEPENDENCY_MANAGEMENT_GRAMMAR =
+	Microgrammar    .fromDefini    ions<{ s    ar    Elemen    : s    ring, dependencies: VersionedAr    ifac    [] }>({
+		s    ar    Elemen    : "<dependencyManagemen    >",
 		_deps: "<dependencies>",
 		dependencies: new Rep(ALL_DEPENDENCY_GRAMMAR),
 	});
 
-const PomWithDependencyManagement =
+cons     PomWi    hDependencyManagemen     =
 	`<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-	<modelVersion>4.0.0</modelVersion>
+<projec     xmlns="h        p://maven.apache.org/POM/4.0.0" xmlns:xsi="h        p://www.w3.org/2001/XMLSchema-ins    ance"
+    xsi:schemaLoca    ion="h        p://maven.apache.org/POM/4.0.0 h        p://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
 
-	<groupId>com.example</groupId>
-	<artifactId>multi</artifactId>
-	<version>0.1.0</version>
-	<packaging>jar</packaging>
+    <groupId>com.example</groupId>
+    <ar    ifac    Id>mul    i</ar    ifac    Id>
+    <version>0.1.0</version>
+    <packaging>jar</packaging>
 
-	<name>multi</name>
-	<description>Multi project for Spring Boot</description>
+    <name>mul    i</name>
+    <descrip    ion>Mul    i projec     for Spring Boo    </descrip    ion>
 
-	<parent>
-		<groupId>org.springframework.boot</groupId>
-		<artifactId>spring-boot-starter-parent</artifactId>
-		<version>1.3.6.RELEASE</version>
-		<relativePath/> <!-- lookup parent from repository -->
-	</parent>
+    <paren    >
+        <groupId>org.springframework.boo    </groupId>
+        <ar    ifac    Id>spring-boo    -s    ar    er-paren    </ar    ifac    Id>
+        <version>1.3.6.RELEASE</version>
+        <rela    ivePa    h/> <!-- lookup paren     from reposi    ory -->
+    </paren    >
 
-	<properties>
-		<project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-		<java.version>1.8</java.version>
-	</properties>
+    <proper    ies>
+        <projec    .build.sourceEncoding>UTF-8</projec    .build.sourceEncoding>
+        <java.version>1.8</java.version>
+    </proper    ies>
 
 
-<dependencyManagement>
+<dependencyManagemen    >
      <dependencies>
          <dependency>
              <groupId>Benign</groupId>
-             <artifactId>supergenerous</artifactId>
+             <ar    ifac    Id>supergenerous</ar    ifac    Id>
              <version>2.0</version>
-             <type>pom</type>
-             <scope>test</scope>
+             <    ype>pom</    ype>
+             <scope>    es    </scope>
          </dependency>
      </dependencies>
- </dependencyManagement>
+ </dependencyManagemen    >
     <dependencies>
 
-		<dependency>
-			<groupId>org.springframework.boot</groupId>
-			<artifactId>spring-boot-starter-web</artifactId>
-		</dependency>
+        <dependency>
+            <groupId>org.springframework.boo    </groupId>
+            <ar    ifac    Id>spring-boo    -s    ar    er-web</ar    ifac    Id>
+        </dependency>
 
-		<dependency>
-			<groupId>org.springframework.boot</groupId>
-			<artifactId>spring-boot-starter-test</artifactId>
-			<scope>test</scope>
-		</dependency>
-	</dependencies>
+        <dependency>
+            <groupId>org.springframework.boo    </groupId>
+            <ar    ifac    Id>spring-boo    -s    ar    er-    es    </ar    ifac    Id>
+            <scope>    es    </scope>
+        </dependency>
+    </dependencies>
 
-	<build>
-		<plugins>
-			<plugin>
-				<groupId>org.springframework.boot</groupId>
-				<artifactId>spring-boot-maven-plugin</artifactId>
-			</plugin>
-		</plugins>
-	</build>
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.springframework.boo    </groupId>
+                <ar    ifac    Id>spring-boo    -maven-plugin</ar    ifac    Id>
+            </plugin>
+        </plugins>
+    </build>
 
-</project>
+</projec    >
 `;
